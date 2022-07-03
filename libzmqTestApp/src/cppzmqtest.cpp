5bcadbc58955f84e43c036b1632a5ccd7ae5214c
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-07-03 16:40:44 +0100

    Add examples

---
+++ libzmqTestApp/src/cppzmqtest.cpp
@@ -0,0 +1,70 @@
+// examples from https://zeromq.org/get-started/
+
+#include <string>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+#include <zmq.hpp>
+
+int cppzmq_server() 
+{
+    using namespace std::chrono_literals;
+
+    // initialize the zmq context with a single IO thread
+    zmq::context_t context{1};
+
+    // construct a REP (reply) socket and bind to interface
+    zmq::socket_t socket{context, zmq::socket_type::rep};
+    socket.bind("tcp://*:5555");
+
+    // prepare some static data for responses
+    const std::string data{"World"};
+
+    for (;;) 
+    {
+        zmq::message_t request;
+
+        // receive a request from client
+        socket.recv(request, zmq::recv_flags::none);
+        std::cout << "Received " << request.to_string() << std::endl;
+
+        // simulate work
+        std::this_thread::sleep_for(1s);
+
+        // send the reply to the client
+        socket.send(zmq::buffer(data), zmq::send_flags::none);
+    }
+
+    return 0;
+}
+
+int cppzmq_client()
+{
+    // initialize the zmq context with a single IO thread
+    zmq::context_t context{1};
+
+    // construct a REQ (request) socket and connect to interface
+    zmq::socket_t socket{context, zmq::socket_type::req};
+    socket.connect("tcp://localhost:5555");
+
+    // set up some static data to send
+    const std::string data{"Hello"};
+
+    for (auto request_num = 0; request_num < 10; ++request_num) 
+    {
+        // send the request message
+        std::cout << "Sending Hello " << request_num << "..." << std::endl;
+        socket.send(zmq::buffer(data), zmq::send_flags::none);
+        
+        // wait for reply from server
+        zmq::message_t reply{};
+        socket.recv(reply, zmq::recv_flags::none);
+
+        std::cout << "Received " << reply.to_string(); 
+        std::cout << " (" << request_num << ")";
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ libzmqTestApp/src/zmqtest.cpp
@@ -0,0 +1,58 @@
+// examples from https://zeromq.org/get-started/
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <zmq.h>
+
+//  Hello World server
+
+int zmq_server (void)
+{
+    //  Socket to talk to clients
+    void *context = zmq_ctx_new ();
+    void *responder = zmq_socket (context, ZMQ_REP);
+    int rc = zmq_bind (responder, "tcp://*:5555");
+    assert (rc == 0);
+
+    while (1) {
+        char buffer [10];
+        zmq_recv (responder, buffer, 10, 0);
+        printf ("Received Hello\n");
+#ifdef _WIN32
+        Sleep (1000);          //  Do some 'work'
+#else
+        sleep (1);          //  Do some 'work'
+#endif
+        zmq_send (responder, "World", 5, 0);
+    }
+    return 0;
+}
+
+//  Hello World client
+
+int zmq_client (void)
+{
+    printf ("Connecting to hello world server…\n");
+    void *context = zmq_ctx_new ();
+    void *requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://localhost:5555");
+
+    int request_nbr;
+    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
+        char buffer [10];
+        printf ("Sending Hello %d…\n", request_nbr);
+        zmq_send (requester, "Hello", 5, 0);
+        zmq_recv (requester, buffer, 10, 0);
+        printf ("Received World %d\n", request_nbr);
+    }
+    zmq_close (requester);
+    zmq_ctx_destroy (context);
+    return 0;
+}
